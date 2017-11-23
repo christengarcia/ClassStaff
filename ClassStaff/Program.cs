d6000226efe42e85d061e5428af935a4dd5d15ea
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-24 09:59:23 +1100

    Added HoursWorked properties.

--- ClassStaff/Program.cs
+++ ClassStaff/Program.cs
@@ -7,6 +7,21 @@ namespace ClassStaff
         private string nameOfStaff;
         private const int hourlyRate = 30;
         private int hWorked;
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                    hWorked = value;
+                else
+                    hWorked = 0;
+            }
+        }
     }
     class Program
     {
