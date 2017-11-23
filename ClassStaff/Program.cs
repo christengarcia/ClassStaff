203ef7ece64c4226e6b2993740c1bd1473517d8f
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-24 09:50:55 +1100

    Added class Staff.

--- ClassStaff/Program.cs
+++ ClassStaff/Program.cs
@@ -2,11 +2,17 @@
 
 namespace ClassStaff
 {
+    class Staff
+    {
+        private string nameOfStaff;
+        private const int hourlyRate = 30;
+        private int hWorked;
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
